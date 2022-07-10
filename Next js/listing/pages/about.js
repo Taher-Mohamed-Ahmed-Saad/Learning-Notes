f841396b335965a3fcceed73efd74ad851813b9f
Taher Mohamed Ahmed Saad <61603695+Taher-Mohamed-Ahmed-Saad@users.noreply.github.com>
2022-07-10 08:12:18 +0200

    Adding next js

---
+++ Next js/listing/pages/about.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+      </div>
+    </>
+  );
+}
+ 
+export default About;
---
+++ Next js/listing/pages/ninjas/[id].js
@@ -0,0 +1,39 @@
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+  
+    // map data to an array of path objects with params (id)
+    const paths = data.map(ninja => {
+      return {
+        params: { id: ninja.id.toString() }
+      }
+    })
+  
+    return {
+      paths,
+      fallback: false
+    }
+  }
+  
+  export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+    const data = await res.json();
+  
+    return {
+      props: { ninja: data }
+    }
+  }
+  
+  const Details = ({ ninja }) => {
+    return (
+      <div>
+        <h1>{ ninja.name }</h1>
+        <p>{ ninja.email }</p>
+        <p>{ ninja.website }</p>
+        <p>{ ninja.address.city }</p>
+      </div>
+    );
+  }
+  
+  export default Details;
